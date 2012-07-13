aa70bb6dc60891046e5009292a05a1526c12ba71
Brenton Cleeland <b.cleeland@gmail.com>
2012-07-13 16:17:03 +1000

    Settings now belong in settings.DJANGO_BASE_TEMPLATE

--- base_processor.py
+++ base_processor.py
@@ -1,9 +1,12 @@
+from django.conf import settings
 
 # JS Addin for base-template
 # If not-blank, enables various Javascript addons
 
 def js_addins(context):
+    base_settings = settings.DJANGO_BASE_TEMPLATE
+    
     return {
-        'GOOGLE_ANALYTICS': 'UA-33187946-1',
-        'TYPEKIT': 'qzn2byi',
+        'GOOGLE_ANALYTICS': base_settings.get('GOOGLE_ANALYTICS', None),
+        'TYPEKIT': base_settings.get('TYPEKIT', None)
     }
