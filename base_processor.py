038ce79024e84a844841e770cf0af00482ed8cb8
Brenton Cleeland <b.cleeland@gmail.com>
2012-07-13 15:42:22 +1000

    Changes to base_processor.py

--- base_processor.py
+++ base_processor.py
@@ -4,6 +4,6 @@
 
 def js_addins(context):
     return {
-        'GOOGLE_ANALYTICS': '',
-        'TYPEKIT_ID': '',
+        'GOOGLE_ANALYTICS': 'UA-33187946-1',
+        'TYPEKIT': 'qzn2byi',
     }
